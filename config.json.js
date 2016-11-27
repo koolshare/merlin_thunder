66a2345ae025c803886cbcabdc5726d710fd6851
lixin <sadoneli@gmail.com>
2016-11-27 20:15:36 +0800

    first commit

---
+++ config.json.js
@@ -0,0 +1,8 @@
+{
+"version":"2.1",
+"md5":"d5bd802d9a80e344a6b826fe72b22d2b",
+"home_url":"Module_thunder.asp",
+"title":"迅雷远程",
+"description":"迅雷远程~",
+"build_date":"2016-11-24_20:56:46"
+}
---
+++ thunder/uninstall.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+
+rm -rf /koolshare/thunder
+rm -rf /init.d/S02Thunder.sh
+rm -rf /koolshare/scripts/thunder_*.sh
+rm -rf /webs/Module_thunder.asp
+
+
