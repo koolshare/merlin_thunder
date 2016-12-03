bc0d5ca62ebea1d993f7e9b8e4e941e56c94ba12
andywoooo <andywoo87630@gmail.com>
2016-12-04 01:10:08 +0800

    uninstall_thunder.sh

---
+++ thunder/scripts/uninstall_thunder.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+rm -rf /koolshare/thunder
+rm -rf /koolshare/init.d/S02Thunder.sh
+rm -rf /koolshare/scripts/thunder_*.sh
+rm -rf /koolshare/scripts/uninstall_thunder.sh
+rm -rf /koolshare/webs/Module_thunder.asp
+PID_all=`ps|grep -E "check_xware_guard|sh(.*)xunlei.sh|thunder|xunlei|xware|sleep 1m|sleep 10m"|grep -v grep|awk '{print $1}'`
+kill ${PID_all}
+
