99ebb1614b9bc71102cffc3082496ac0c1ab05a4
andywoooo <andywoo87630@gmail.com>
2016-12-06 21:19:56 +0800

    update install.sh

--- thunder/install.sh
+++ thunder/install.sh
@@ -1,10 +1,17 @@
 #! /bin/sh
 cd /tmp
-cp -rf /tmp/thunder/thunder /koolshare/
+if [ -e /koolshare/thunder/lib ]; then
+	cp -rf /tmp/thunder/thunder/*.sh /koolshare/thunder/
+else
+	cp -rf /tmp/thunder/thunder /koolshare/
+fi
 cp -rf /tmp/thunder/scripts/* /koolshare/scripts/
 cp -rf /tmp/thunder/webs/* /koolshare/webs/
 cp -rf /tmp/thunder/init.d/* /koolshare/init.d/
 cp -rf /tmp/thunder/res/* /koolshare/res/
+PID_all=`ps|grep -E "thunder|xunlei|xware|sleep 1m|sleep 10m"|grep -v grep|awk '{print $1}'`
+kill ${PID_all}
+sh /koolshare/thunder/*.sh
 cd /
 rm -rf /tmp/thunder* >/dev/null 2>&1
 
