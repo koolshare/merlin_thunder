9667fc4aa49506e3293947e20c34d9163e8531ff
andywoooo <andywoo87630@gmail.com>
2016-12-06 21:58:26 +0800

    update all

--- thunder/install.sh
+++ thunder/install.sh
@@ -1,25 +1,23 @@
-#! /bin/sh
-cd /tmp
+#!/bin/sh
+#
 if [ -e /koolshare/thunder/lib ]; then
 	cp -rf /tmp/thunder/thunder/*.sh /koolshare/thunder/
 else
 	cp -rf /tmp/thunder/thunder /koolshare/
 fi
-cp -rf /tmp/thunder/scripts/* /koolshare/scripts/
-cp -rf /tmp/thunder/webs/* /koolshare/webs/
-cp -rf /tmp/thunder/init.d/* /koolshare/init.d/
-cp -rf /tmp/thunder/res/* /koolshare/res/
-PID_all=`ps|grep -E "thunder|xunlei|xware|sleep 1m|sleep 10m"|grep -v grep|awk '{print $1}'`
-kill ${PID_all}
-sh /koolshare/thunder/*.sh
+cp -rf /tmp/thunder/scripts /koolshare
+cp -rf /tmp/thunder/webs /koolshare
+cp -rf /tmp/thunder/init.d /koolshare
+cp -rf /tmp/thunder/res /koolshare
+
 cd /
 rm -rf /tmp/thunder* >/dev/null 2>&1
 
-if [ -f /koolshare/init.d/S70Thunder.sh ];then
+if [ -f /koolshare/init.d/S70Thunder.sh ]; then
 	rm -rf /koolshare/init.d/S70Thunder.sh
 fi
 
-if [ -L /koolshare/init.d/S70Thunder.sh ];then
+if [ -L /koolshare/init.d/S70Thunder.sh ]; then
 	rm -rf /koolshare/init.d/S70Thunder.sh
 fi
 
@@ -30,3 +28,5 @@ chmod 755 /koolshare/thunder/*
 chmod 755 /koolshare/scripts/*
 chmod 755 /koolshare/init.d/*
 
+PID_all=`ps|grep -E "check_xware_guard|sh(.*)xunlei.sh|thunder|xunlei|xware|sleep 1m|sleep 10m"|grep -v grep|awk '{print $1}'`
+kill ${PID_all}
