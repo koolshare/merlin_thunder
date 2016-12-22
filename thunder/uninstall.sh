f8872e1acb20686708fb90ff72368562e47971fa
andywoooo <andywoo87630@gmail.com>
2016-12-22 23:41:23 +0800

    update

--- thunder/uninstall.sh
+++ thunder/uninstall.sh
@@ -13,7 +13,7 @@ rm -rf /koolshare/init.d/S02Thunder.sh
 rm -rf /koolshare/scripts/thunder_*.sh
 rm -rf /koolshare/scripts/uninstall_thunder.sh
 rm -rf /koolshare/webs/Module_thunder.asp
-#rm -rf /tmp/thunder*
+rm -rf /tmp/thunder*
 rm -rf /tmp/xunlei*
 PID_all=`process_of 'EmbedThunderManager|ETMDaemon|vod_httpserver|check_xware_guard|thunder|xunlei|xware|sleep 1m|sleep 10m'|awk '{print $1}'`
 until [ -z `process_of 'EmbedThunderManager|ETMDaemon|vod_httpserver|check_xware_guard|thunder|xunlei|xware'` ]; do
